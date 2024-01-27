802b862adf6e463a3d927c7e3bb58961220d20a3
Jatin Vashisht <jatinvashisht11@gmail.com>
2024-01-27 17:50:05 +0530

    Send and Receive Messages done

--- client/src/components/Chat/MessageBar.jsx
+++ client/src/components/Chat/MessageBar.jsx
@@ -1,9 +1,40 @@
+import { useStateProvider } from "@/context/StateContext"
+import { reducerCases } from "@/context/constants"
+import { ADD_MESSAGE_ROUTE } from "@/utils/ApiRoutes"
+import axios from "axios"
+import { useState } from "react"
 import { BsEmojiSmile } from "react-icons/bs"
 import { FaMicrophone } from "react-icons/fa"
 import {ImAttachment} from 'react-icons/im'
 import { MdSend } from "react-icons/md"
 
 const MessageBar = () => {
+  const [{userInfo, currentChatUser, socket}, dispatch] = useStateProvider();
+  const [message, setMessage] = useState('')
+  const sendMessage = async () => {
+    try {
+      const {data} = await axios.post(ADD_MESSAGE_ROUTE, {
+        to: currentChatUser?.id,
+        from: userInfo?.id,
+        message
+      })
+      socket.current.emit('send-msg', {
+        to: currentChatUser?.id,
+        from: userInfo?.id,
+        message: data.message
+      })
+      dispatch({
+        type: reducerCases.ADD_MESSAGE,
+        newMessage: {
+          ...data.message
+        },
+        fromSelf: true
+      })
+      setMessage('')
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className="bg-panel-header-background h-20 px-4 flex items-center gap-6 relative">
       <>
@@ -12,11 +43,11 @@ const MessageBar = () => {
           <ImAttachment className="text-panel-header-icon cursor-pointer text-xl" title="Attach File" />
         </div>
         <div className="w-full rounded-lg h-10 flex items-center">
-          <input type="text" placeholder="Type a message" className="bg-input-background text-sm focus:outline-none text-white h-10 rounded-lg px-5 py-4 w-full" />
+          <input type="text" placeholder="Type a message" className="bg-input-background text-sm focus:outline-none text-white h-10 rounded-lg px-5 py-4 w-full" value={message} onChange={e => setMessage(e.target.value)} onKeyDown={e => {if(e.key === 'Enter') sendMessage()}} />
         </div>
         <div className="flex w-10 items-center justify-center">
           <button>
-            <MdSend className="text-panel-header-icon cursor-pointer text-xl" title="Send message" />
+            <MdSend onClick={sendMessage} className="text-panel-header-icon cursor-pointer text-xl" title="Send message" />
             {/* <FaMicrophone className="text-panel-header-icon cursor-pointer text-xl" title="Record" /> */}
           </button>
         </div>
--- client/src/components/common/MessageStatus.jsx
+++ client/src/components/common/MessageStatus.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react'
+import { BsCheck, BsCheckAll } from 'react-icons/bs'
 
-function MessageStatus() {
-  return <div>MessageStatus</div>;
+const MessageStatus = ({messageStatus}) => {
+  return (
+    <>
+      {messageStatus === 'sent' && <BsCheck className='text-lg' />}
+      {messageStatus === 'delivered' && <BsCheckAll className='text-lg' />}
+      {messageStatus === 'read' && <BsCheckAll className='text-lg text-icon-ack' />}
+    </>
+  )
 }
 
-export default MessageStatus;
+export default MessageStatus
