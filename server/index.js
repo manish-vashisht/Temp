a7fb400c6db58ed1676023016b4de896f6129f2b
manish-vashisht <142409805+manish-vashisht@users.noreply.github.com>
2024-01-29 07:06:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,9 +9,14 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.CLIENT_HOST,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    optionsSuccessStatus: 200
+}))
 app.use(express.json())
-
+    
 app.use('/uploads/recordings', express.static('uploads/recordings'))
 app.use('/uploads/images', express.static('uploads/images'))
 
