1900c837c4c545824b8c3f264b7a0a3814bc7c11
manish-vashisht <142409805+manish-vashisht@users.noreply.github.com>
2024-01-31 21:42:02 +0530

    Update ChatContainer.jsx

--- client/src/components/Chat/ChatContainer.jsx
+++ client/src/components/Chat/ChatContainer.jsx
@@ -11,7 +11,7 @@ const ChatContainer = () => {
   return (
     <div className="h-[80vh] w-full relative flex-grow overflow-auto custom-scrollbar">
       <div className="bg-chat-background bg-fixed h-full w-full opacity-5 fixed left-0 top-0 z-0"></div>
-      <div className="mx-10 my-6 relative bottom-0 z-40 left-0">
+      <div className="mx-3 my-3 sm:mx-10 sm:my-6 relative bottom-0 z-40 left-0">
         <div className="flex w-full">
           <div className="flex flex-col justify-end w-full gap-1 overflow-auto">
             {messages.map((message, i) => (
