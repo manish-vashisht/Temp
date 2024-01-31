dbf2bd2862d8e428d7d871a97005ff48354c3702
manish-vashisht <142409805+manish-vashisht@users.noreply.github.com>
2024-01-31 21:47:30 +0530

    Update Empty.jsx

--- client/src/components/Empty.jsx
+++ client/src/components/Empty.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Empty = () => {
   return (
-    <div className='border-conversation-border border-l w-full bg-panel-header-background flex flex-col h-[100vh] border-b-4 border-b-icon-green items-center justify-center'>
+    <div className='hidden sm:flex border-conversation-border border-l w-full bg-panel-header-background flex flex-col h-[100vh] border-b-4 border-b-icon-green items-center justify-center'>
       <Image src='/whatsapp.gif' alt='Whatsapp' height={300} width={300} priority />
     </div>
   )
