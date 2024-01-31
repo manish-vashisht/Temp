1087d182e67bb86d91ed998cebdfc69d9939b1e5
manish-vashisht <142409805+manish-vashisht@users.noreply.github.com>
2024-01-31 21:42:34 +0530

    Update ImageMessage.jsx

--- client/src/components/Chat/ImageMessage.jsx
+++ client/src/components/Chat/ImageMessage.jsx
@@ -10,7 +10,7 @@ const ImageMessage = ({message}) => {
   return (
     <div className={`p-1 rounded-lg ${message.senderId === currentChatUser.id ? 'bg-incoming-background' : 'bg-outgoing-background'}`}>
       <div className="relative">
-        <Image src={`${HOST}/${message.message}`} className='rounded-lg' alt='asset' height={300} width={300} />
+        <Image src={`${HOST}/${message.message}`} className='rounded-lg' alt='asset' height={200} width={200} />
         <div className="absolute bottom-1 right-1 flex items-end gap-1">
           <span className='text-bubble-meta text-[11px] pt-1 min-w-fit'>
             {calculateTime(message.createdAt)}
