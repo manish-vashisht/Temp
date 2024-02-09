26b292ff5f8821769c5adeb42f88d99aaa096153
manish-vashisht <142409805+manish-vashisht@users.noreply.github.com>
2024-02-09 19:34:15 +0530

    Update Chat.jsx

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -5,7 +5,7 @@ import MessageBar from './MessageBar';
 
 const Chat = () => {
   return (
-    <div className={`border-conversation-border border-l w-full bg-conversation-panel-background flex flex-col z-10 `}>
+    <div className={`border-conversation-border border-l w-full bg-conversation-panel-background flex flex-col z-10 h-screen`}>
       <ChatHeader />
       <ChatContainer />
       <MessageBar />
